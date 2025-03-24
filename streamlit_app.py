a90da78d063424d4c70581060924231b9a9c426b
Lence Majzovska <lence.maj@gmail.com>
2025-03-24 15:25:44 +0100

    Fixat fel i koden

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ st.set_page_config(
     page_title="Sifferklassificering",
     page_icon="🔢",
     layout="wide",
-    initial_sidebar_state="expanded"
+    initial_sidebar_state="expanded",
     theme="dark"
 )
 
